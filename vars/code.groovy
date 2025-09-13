fc680ce262673be72f98a04b6c0eecdc65b236d0
BabuLahade <babulahade@gmail.com>
2025-09-13 12:04:26 +0530

    Create code.groovy

---
+++ vars/code.groovy
@@ -0,0 +1,19 @@
+def call(String url , String branch{
+   echo "This is cloning the code"
+   git url: "${GitUrl}, branch: "${branch}"
+   echo "Code cloned successfully"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
